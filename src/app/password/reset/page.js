42c6a3d772f952d8c91d85f6e1ae177e0ef8222a
Dương Tùng Anh <tunnaduong@gmail.com>
2024-10-31 09:19:07 +0700

    fix import error

--- src/app/password/reset/page.js
+++ src/app/password/reset/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Loader2, MailCheck } from "lucide-react";
+import { Loader2, MailCheck, AlertCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
