a55211cd5f72411a61bc1c196cbff384385a95c4
Dương Tùng Anh <tunnaduong@gmail.com>
2024-11-03 16:31:18 +0700

    Refactor meta tags in layout.js

--- src/app/forum/page.js
+++ src/app/forum/page.js
@@ -1,7 +1,9 @@
 import Navbar from "@/components/include/navbar";
 import LeftSidebar from "@/components/include/leftSidebar";
 import RightSidebar from "@/components/include/rightSidebar";
-import { IoChatbubblesSharp, IoCheckmarkCircle } from "react-icons/io5";
+import { IoChatbubblesSharp } from "react-icons/io5";
+import { HiBadgeCheck } from "react-icons/hi";
+import Link from "next/link";
 
 export default function Forum() {
   return (
@@ -20,12 +22,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Trung học phổ thông
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">28</span>{" "}
@@ -41,13 +43,18 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       Cách khoanh bừa mà vẫn trúng 100% uy tín
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">Hoàng Phát</span>
-                    <IoCheckmarkCircle className="text-[11px] leading-5 ml-0.5" />
+                    <Link href="#" className="text-[10px]">
+                      Hoàng Phát
+                    </Link>
+                    <HiBadgeCheck className="text-[11px] leading-5 ml-0.5" />
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
@@ -58,12 +65,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Trung học cơ sở
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">14</span>{" "}
@@ -79,12 +86,17 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       Cho em hỏi học toán như nào để giỏi hơn ạ?
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">Nguyễn Đặng Hải</span>
+                    <Link href="#" className="text-[10px]">
+                      Nguyễn Đặng Hải
+                    </Link>
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
@@ -95,12 +107,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Tiếng Anh
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">9</span>{" "}
@@ -116,13 +128,18 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       [THÔNG BÁO] Tổ chức cuộc thi Tiếng Anh
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">Admin</span>
-                    <IoCheckmarkCircle className="text-[11px] leading-5 ml-0.5" />
+                    <Link href="#" className="text-[10px]">
+                      Admin
+                    </Link>
+                    <HiBadgeCheck className="text-[11px] leading-5 ml-0.5" />
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
@@ -133,12 +150,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Ebook - Giáo trình
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">47</span>{" "}
@@ -154,13 +171,18 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       [CHIA SẺ] 100 ebook miễn phí dành cho học sinh, sinh viên
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">Dương Tùng Anh</span>
-                    <IoCheckmarkCircle className="text-[11px] leading-5 ml-0.5" />
+                    <Link href="#" className="text-[10px]">
+                      Dương Tùng Anh
+                    </Link>
+                    <HiBadgeCheck className="text-[11px] leading-5 ml-0.5" />
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
@@ -177,12 +199,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Âm nhạc
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">28</span>{" "}
@@ -198,13 +220,18 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       List nhạc EDM cực xung
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">Đức Duy N.</span>
-                    <IoCheckmarkCircle className="text-[11px] leading-5 ml-0.5" />
+                    <Link href="#" className="text-[10px]">
+                      Đức Duy N.
+                    </Link>
+                    <HiBadgeCheck className="text-[11px] leading-5 ml-0.5" />
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
@@ -215,12 +242,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Hình ảnh đẹp
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">28</span>{" "}
@@ -236,12 +263,17 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       100 hình ảnh từ cuộc sống
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">Anh em ơi</span>
+                    <Link href="#" className="text-[10px]">
+                      Anh em ơi
+                    </Link>
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
@@ -252,12 +284,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Thư giãn - Đố vui
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">28</span>{" "}
@@ -273,13 +305,18 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       Tuyển tập ảnh gái xinh
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">MOD4R</span>
-                    <IoCheckmarkCircle className="text-[11px] leading-5 ml-0.5" />
+                    <Link href="#" className="text-[10px]">
+                      MOD4R
+                    </Link>
+                    <HiBadgeCheck className="text-[11px] leading-5 ml-0.5" />
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
@@ -290,12 +327,12 @@ export default function Forum() {
               <div className="flex flex-row items-center">
                 <IoChatbubblesSharp className="text-[#319528] text-[23px] m-4" />
                 <div className="flex flex-col flex-1">
-                  <a
+                  <Link
                     href="#"
                     className="text-[#319528] text-[12px] font-bold w-fit"
                   >
                     Thế giới Game
-                  </a>
+                  </Link>
                   <span className="text-[10px] text-gray-500">
                     Bài viết:{" "}
                     <span className="mr-1 font-semibold text-black">28</span>{" "}
@@ -311,13 +348,18 @@ export default function Forum() {
                     <span className="text-[10px] whitespace-nowrap mr-1">
                       Mới nhất:
                     </span>
-                    <span className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden">
+                    <Link
+                      href="#"
+                      className="text-[10px] text-[#319528] inline-block leading-3 text-ellipsis whitespace-nowrap overflow-hidden"
+                    >
                       [REVIEW] GTA V PC
-                    </span>
+                    </Link>
                   </div>
                   <div className="leading-3 flex items-center mt-1 text-[#319528]">
-                    <span className="text-[10px]">Dương Tùng Anh</span>
-                    <IoCheckmarkCircle className="text-[11px] leading-5 ml-0.5" />
+                    <Link href="#" className="text-[10px]">
+                      Dương Tùng Anh
+                    </Link>
+                    <HiBadgeCheck className="text-[11px] leading-5 ml-0.5" />
                     <span className="text-[10px] text-black">
                       , 1 ngày trước
                     </span>
--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,10 @@ export default function RootLayout({ children }) {
           type="image/png"
           sizes="32x32"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0"
+        ></meta>
         <title>Thanh niên Chuyên Biên Hòa Online</title>
       </head>
       <body className="bg-[#F8F8F8]">
