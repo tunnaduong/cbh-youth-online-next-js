eabd9f7a5416920441747a0eed2d71c3b95ab282
huanhoahongso3-collab <huanhoahongso3@gmail.com>
2026-06-17 09:04:55 +0700

    rollback

--- src/app/loading.js
+++
@@ -1,9 +0,0 @@
-import LoadingScreen from "@/components/ui/LoadingScreen";
-
-export default function Loading() {
-  return (
-    <LoadingScreen isLoading={true}>
-      <div className="w-full min-h-screen bg-[#F8F8F8] dark:bg-neutral-800" />
-    </LoadingScreen>
-  );
-}
